53a40586c21c5ac706b5de884290d3e66eab5e3b
Celal Ceken <celalceken@gmail.com>
2016-11-05 18:47:56 +0200

    Basic SQL (SQL DML Commands; Select, Insert, Update, Delete, Join)

--- VeritabaniYonetimSistemleri6/SQLSorgulari6.sql
+++ VeritabaniYonetimSistemleri6/SQLSorgulari6.sql
@@ -40,13 +40,13 @@ SELECT "UnitPrice", "UnitPrice"*1.18 as "KDVliTutar"  from products;
 SELECT "OrderID" as "SiparisNo", "ShipPostalCode"||','||"ShipAddress" as "GonderiAdresi" FROM "orders"   
 WHERE "OrderDate" BETWEEN '07/04/1996' AND '07/09/1996'; 
  
--------------------
+--------------------------------------
+
 SELECT   "public"."orders"."OrderID" as "Siparis No",
          "public"."customers"."CompanyName" as "Şirket",
          "public"."orders"."OrderDate" as "Sipariş Tarihi"
 FROM     "orders" 
 INNER JOIN "customers"  ON "orders"."CustomerID" = "customers"."CustomerID" 
--------------------
 SELECT   "public"."orders"."OrderID" as "Siparis No",
          "public"."customers"."CompanyName" as "Şirket",
          "public"."orders"."OrderDate" as "Sipariş Tarihi"
@@ -54,7 +54,6 @@ FROM     "orders",  "customers"
 where "orders"."CustomerID" = "customers"."CustomerID" 
 order by "customers"."CompanyName" DESC;
 
--------------------
 
 SELECT   "public"."orders"."OrderID",
          "public"."orders"."OrderDate",
@@ -66,19 +65,33 @@ INNER JOIN "customers"  ON "orders"."CustomerID" = "customers"."CustomerID"
 INNER JOIN "employees"  ON "orders"."EmployeeID" = "employees"."EmployeeID" 
 
 
+
+SELECT   "public"."orders"."OrderID",
+         "public"."products"."ProductName"
+FROM     "order_details" 
+INNER JOIN "orders"  ON "order_details"."OrderID" = "orders"."OrderID" 
+INNER JOIN "products"  ON "order_details"."ProductID" = "products"."ProductID”
+
+
 -----------------------------
+
 SELECT   "public"."orders"."OrderID" as "Siparis No",
          "public"."customers"."CompanyName" as "Şirket",
          "public"."orders"."OrderDate" as "Sipariş Tarihi"
 FROM     "customers"
 LEFT OUTER JOIN "orders" on "orders"."CustomerID" = "customers"."CustomerID" ;
+
 ---------------
+
 SELECT   "public"."orders"."OrderID" as "Siparis No",
          "public"."employees"."FirstName" as "Satış Temsilcisi Ad",
 		 "public"."employees"."LastName" as "Satış Temsilcisi Soyad",
          "public"."orders"."OrderDate" as "Sipariş Tarihi"
 FROM     "orders"
 RIGHT OUTER JOIN "employees" on "orders"."EmployeeID" = "employees"."EmployeeID" ;
+
+-------------------
+
 
 select "CompanyName","ContactName" INTO "MusteriYedek02" from customers;
 
