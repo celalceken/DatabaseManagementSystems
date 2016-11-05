8942ff5065e5bced074511fb91c17d86b4e8dd1a
Veysel Harun Sahin <vsahin@sakarya.edu.tr>
2016-11-05 23:50:13 +0300

    Biçimsel düzeltmeler yapıldı. Yeni birkaç sorgu eklendi.

--- VeritabaniYonetimSistemleri7/SQLSorgulari7.sql
+++ VeritabaniYonetimSistemleri7/SQLSorgulari7.sql
@@ -1,97 +1,168 @@
-CREATE DATABASE "AlisVerisUygulamasi1" ENCODING='UTF-8' 
-LC_COLLATE='tr_TR.UTF-8' LC_CTYPE='tr_TR.UTF-8'
-OWNER postgres 
+
+
+CREATE DATABASE "AlisVerisUygulamasi"
+ENCODING='UTF-8'
+LC_COLLATE='tr_TR.UTF-8'
+LC_CTYPE='tr_TR.UTF-8'
+OWNER postgres
 TEMPLATE=template0;
 
+
+-------------------------------
 
 CREATE SCHEMA sema1;
 
 
-CREATE TABLE "sema1"."Urunler" ( 
-	"urunNo" serial,
-	"kodu" Char( 6 ) NOT NULL,
-	"adi" Varchar( 40 )  NOT NULL,
-	"uretimTarihi" Date DEFAULT '2000-01-01',
-	"birimFiyati" Money,
-	"miktari" SmallInt DEFAULT '0',
-	CONSTRAINT "urunlerPK" PRIMARY KEY ( "urunNo" ),
-	CONSTRAINT "urunlerUnique1" UNIQUE( "kodu" ),
-	CONSTRAINT "urunlerCheck1" CHECK(miktari >= 0) 
+-------------------------------
+
+
+CREATE TABLE "sema1"."Urunler" (
+	"urunNo" SERIAL,
+	"kodu" CHAR(6) NOT NULL,
+	"adi" VARCHAR(40) NOT NULL,
+	"uretimTarihi" DATE DEFAULT '2017-01-01',
+	"birimFiyati" MONEY,
+	"miktari" SMALLINT DEFAULT '0',
+	CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+	CONSTRAINT "urunlerUnique" UNIQUE("kodu"),
+	CONSTRAINT "urunlerCheck" CHECK(miktari >= 0)
+);
+
+INSERT INTO "sema1"."Urunler"
+("kodu", "adi", "birimFiyati", "uretimTarihi", "miktari")
+VALUES
+('ELO004', 'TV', '13', '24.10.2016', 5);
+
+
+-------------------------------
+
+
+CREATE TABLE "Urunler" (
+	"urunNo" INTEGER,
+	"kodu" CHAR(6) NOT NULL,
+	"adi" VARCHAR(40) NOT NULL,
+	"uretimTarihi" DATE DEFAULT '2017-01-01',
+	"birimFiyati" MONEY,
+	"miktari" SMALLINT DEFAULT '0',
+	CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+	CONSTRAINT "urunlerUnique" UNIQUE("kodu"),
+	CONSTRAINT "urunlerCheck" CHECK(miktari >= 0)
+);
+
+CREATE SEQUENCE "sayac";
+
+SELECT NEXTVAL('sayac');
+
+ALTER SEQUENCE "sayac" OWNED BY "Urunler"."urunNo";
+
+INSERT INTO "sema1"."Urunler"
+("urunNo", "kodu", "adi", "birimFiyati", "uretimTarihi", "miktari")
+VALUES
+(NEXTVAL('sayac'), 'ELO004', 'TV', '13', '24.10.2016', 5);
+
+
+-------------------------------
+
+
+CREATE SEQUENCE "sayac";
+
+CREATE TABLE "Urunler" (
+	"urunNo" INTEGER NEXTVAL('sayac'),
+	"kodu" CHAR(6) NOT NULL,
+	"adi" VARCHAR(40) NOT NULL,
+	"uretimTarihi" DATE DEFAULT '2017-01-01',
+	"birimFiyati" MONEY,
+	"miktari" SMALLINT DEFAULT '0',
+	CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+	CONSTRAINT "urunlerUnique" UNIQUE("kodu"),
+	CONSTRAINT "urunlerCheck" CHECK(miktari >= 0)
 );
 
 
-SELECT NEXTVAL('sequence1')
+-------------------------------
 
-INSERT INTO "sema1"."Urunler" ("urunNo","kodu", "adi", "birimFiyati", "uretimTarihi", "miktari")
- VALUES(nextval('sequence1'::regclass),'ELO004', 'TV', '13', '1.1.2000', 5)
 
- 
- CREATE TABLE "public"."Urunler3"
-(
-    urunNo INTEGER,
-    kodu CHAR(6) NOT NULL,
-    adi varchar(40) NOT NULL,
-    uretimTarihi  date default '1.1.2000',
-    birimFiyati money,
-    miktari SMALLINT default 0,
-    CONSTRAINT UrunlerPK1 PRIMARY KEY(urunNo),
-    CONSTRAINT UrunlerUnique2 UNIQUE (kodu),
-    CONSTRAINT UrunlerCheck1 CHECK (miktari>=0)
- )
-
--------------------
+--RESTRICT varsayılan ayar
 
 ALTER TABLE "orders"
-	ADD CONSTRAINT "lnk_employees_orders" FOREIGN KEY ( "EmployeeID" )
-	REFERENCES "employees" ( "EmployeeID" ) MATCH FULL
-	ON DELETE Restrict
-	ON UPDATE Restrict;	
+ADD CONSTRAINT "lnk_employees_orders" FOREIGN KEY ("EmployeeID")
+REFERENCES "employees"("EmployeeID")
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
 
--------------------
-CREATE TABLE "public"."Musteriler" ( 
-	"musteriNo" Serial NOT NULL,
-	"adi" Character Varying( 40 ) COLLATE "pg_catalog"."default" NOT NULL,
-	"soyadi" Character Varying( 40 ) COLLATE "pg_catalog"."default" NOT NULL,
-	PRIMARY KEY ( "musteriNo" ) );
- 
-CREATE INDEX "index_adi" ON "public"."Musteriler" USING btree( "adi" );
+ALTER TABLE "orders"
+ADD CONSTRAINT "lnk_employees_orders" FOREIGN KEY ("EmployeeID")
+REFERENCES "employees"("EmployeeID")
+ON DELETE CASCADE
+ON UPDATE CASCADE;
 
-
-CREATE INDEX "index_soyadi" ON "public"."Musteriler" USING btree( "soyadi" );
-
------------
-ALTER TABLE "SiparisDetay" 
-ADD CONSTRAINT "UrunSiparisUnique" UNIQUE ("urunNo","siparisNo");
------
+ALTER TABLE "orders"
+ADD CONSTRAINT "lnk_employees_orders" FOREIGN KEY ("EmployeeID")
+REFERENCES "employees" ("EmployeeID")
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
 
-select sum("UnitPrice")/count("ProductID") from "products"
+-------------------------------
 
-select avg("UnitPrice") from "products"
-select * from "products" order by "ProductID" DESC limit 4
+
+CREATE TABLE "Musteriler" (
+	"musteriNo" SERIAL NOT NULL,
+	"adi" CHARACTER VARYING(40) COLLATE "pg_catalog"."default" NOT NULL,
+	"soyadi" CHARACTER VARYING(40) COLLATE "pg_catalog"."default" NOT NULL,
+	CONSTRAINT "musteriNoPK" PRIMARY KEY ("musteriNo")
+);
+
+CREATE INDEX "indexAdi" ON "Musteriler" ("adi");
+
+CREATE INDEX "indexSoyadi" ON "Musteriler" USING btree ("soyadi");
+
+
+-------------------------------
+
+
+ALTER TABLE "SiparisDetay"
+ADD CONSTRAINT "UrunSiparisUnique" UNIQUE ("urunNo", "siparisNo");
+
+
+-------------------------------
+
+
+SELECT SUM("UnitPrice") / COUNT("ProductID") FROM "products";
+
+SELECT AVG("UnitPrice") FROM "products";
+
+SELECT * FROM "products" ORDER BY "ProductID" DESC LIMIT 4
+
+
 --------
-select "CategoryID", count("CategoryID") as "Ürün Sayısı" from "products"
-group by "CategoryID"
-
-select "CategoryID", sum("UnitPrice") as "Toplam B. Fiyat" from "products"
-group by "CategoryID"
 
 
-select "CategoryID", count("CategoryID") as "Ürün Sayısı" from "products"
-group by "CategoryID"
-having count("CategoryID")>7
+SELECT "CategoryID", COUNT("CategoryID") AS "Ürün Sayısı"
+FROM "products"
+GROUP BY "CategoryID"
 
+SELECT "CategoryID", SUM("UnitPrice") AS "Toplam B. Fiyat"
+FROM "products"
+GROUP BY "CategoryID"
 
-select "CategoryID", count("CategoryID") as "Ürün Sayısı" from "products"
-group by "CategoryID"
-having "CategoryID"=2
+SELECT "CategoryID", COUNT("CategoryID") AS "Ürün Sayısı"
+FROM "products"
+GROUP BY "CategoryID"
+HAVING COUNT("CategoryID") > 7;
 
-select "Country", count("CustomerID") as "Müşteri Sayısı" from "customers"
-group by "Country"
-having count("CustomerID")>5
+SELECT "CategoryID", COUNT("CategoryID") AS "Ürün Sayısı"
+FROM "products"
+GROUP BY "CategoryID"
+HAVING "CategoryID"=2;
 
-SELECT   "public"."customers"."CompanyName", count("OrderID")
-FROM     "orders" 
-INNER JOIN "customers"  ON "orders"."CustomerID" = "customers"."CustomerID" 
-group by "CompanyName" 
-order by "CompanyName" 
+SELECT "Country", COUNT("CustomerID") AS "Müşteri Sayısı"
+FROM "customers"
+GROUP BY "Country"
+HAVING COUNT("CustomerID") > 5;
+
+SELECT "customers"."CompanyName", COUNT("OrderID")
+FROM "orders"
+INNER JOIN "customers" ON "orders"."CustomerID" = "customers"."CustomerID"
+GROUP BY "CompanyName"
+ORDER BY "CompanyName";
