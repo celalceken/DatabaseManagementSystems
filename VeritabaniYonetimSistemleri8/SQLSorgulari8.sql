715e6f090ed581140a816191c309a8fc708b4841
VEYSEL HARUN ŞAHİN <Veysel@VEYSELs-MacBook-Pro.local>
2016-11-21 15:04:33 +0300

    Biçimsel düzenlemeler yapıldı.

--- VeritabaniYonetimSistemleri8/SQLSorgulari8.sql
+++ VeritabaniYonetimSistemleri8/SQLSorgulari8.sql
@@ -1,107 +1,118 @@
-——————Kalıtım—————
+
+
+-- Kalıtım Örneği
+
 
 ALTER TABLE "personel"."Danisman"
-	ADD CONSTRAINT "DanismanPersonel" FOREIGN KEY ( "personelNo" )
-	REFERENCES "personel"."Personel" ( "personelNo" ) MATCH FULL
-	ON DELETE Cascade
-	ON UPDATE Cascade;
+	ADD CONSTRAINT "DanismanPersonel" FOREIGN KEY ("personelNo")
+	REFERENCES "personel"."Personel" ("personelNo")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
+
 
 ALTER TABLE "personel"."SatisTemsilcisi"
-	ADD CONSTRAINT "SatisTemsilcisiPersonel" FOREIGN KEY ( "personelNo" )
-	REFERENCES "personel"."Personel" ( "personelNo" ) MATCH FULL
-	ON DELETE Cascade
-	ON UPDATE Cascade;
+	ADD CONSTRAINT "SatisTemsilcisiPersonel" FOREIGN KEY ("personelNo")
+	REFERENCES "personel"."Personel" ("personelNo")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
 
 
-select * from "personel"."Personel"
-inner join "personel"."SatisTemsilcisi" 
-on "personel"."Personel"."personelNo"= "personel"."SatisTemsilcisi"."personelNo"
+SELECT * FROM "personel"."Personel"
+INNER JOIN "personel"."SatisTemsilcisi" 
+ON "personel"."Personel"."personelNo" = "personel"."SatisTemsilcisi"."personelNo"
 
 
-select adi,soyadi from "personel"."Personel" 
-where personelTipi=’S’;
+SELECT "adi", "soyadi" FROM "personel"."Personel" 
+WHERE "personelTipi"='S';
 
-———View—————
+
+------------------------------
+
+
+-- Görünüm / View Örneği
+
 
 CREATE OR REPLACE VIEW "public"."OrderCustomerEmployee" AS  
-SELECT orders."OrderID",
-    orders."OrderDate",
-    customers."CompanyName",
-    customers."ContactName",
-    employees."FirstName",
-    employees."LastName"
-FROM ((orders
-     JOIN employees ON ((orders."EmployeeID" = employees."EmployeeID")))
-     JOIN customers ON ((orders."CustomerID" = customers."CustomerID")));
+SELECT "orders"."OrderID",
+    "orders"."OrderDate",
+    "customers"."CompanyName",
+    "customers"."ContactName",
+    "employees"."FirstName",
+    "employees"."LastName"
+FROM "orders" 
+INNER JOIN "employees" ON "orders"."EmployeeID" = "employees"."EmployeeID"
+INNER JOIN "customers" ON "orders"."CustomerID" = "customers"."CustomerID";
 
 
+------------------------------
 
 
-———————Özyineli Birleştirme/ Tekli İlişki—————
+-- Özyineli Birleştirme / Tekli İlişki Örneği
+
 
 ALTER TABLE "employees"
-	ADD CONSTRAINT "lnk_employees_employees" FOREIGN KEY ( "ReportsTo" )
-	REFERENCES "employees" ( "EmployeeID" ) MATCH FULL
-	ON DELETE Cascade
-	ON UPDATE Cascade;
----------------------
-SELECT   "public"."employees"."FirstName",
-         "public"."employees"."LastName",
-         "Yonetici"."FirstName",
-         "Yonetici"."LastName"
-FROM     "employees" 
-INNER JOIN "employees" AS "Yonetici"  ON "Yonetici"."EmployeeID" = "employees"."ReportsTo"
+	ADD CONSTRAINT "lnk_employees_employees" FOREIGN KEY ("ReportsTo")
+	REFERENCES "employees" ("EmployeeID")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE;
 
 
+SELECT "public"."employees"."FirstName",
+    "public"."employees"."LastName",
+    "Yonetici"."FirstName",
+    "Yonetici"."LastName"
+FROM "employees" 
+INNER JOIN "employees" AS "Yonetici" ON "Yonetici"."EmployeeID" = "employees"."ReportsTo"
 
 
+------------------------------
 
 
-——————————Alt Sorgu —————-
+-- Alt Sorgu Örnekleri
 
 
+-- Örnek
 
 
-
-select avg("UnitPrice") From "products"
-
-select "ProductID","UnitPrice" From "products"
-where "UnitPrice"< (select avg("UnitPrice") From "products")
+SELECT AVG("UnitPrice") FROM "products";
 
 
+SELECT "ProductID", "UnitPrice" FROM "products"
+WHERE "UnitPrice" < (SELECT AVG("UnitPrice") FROM "products");
 
 
-
-select "ProductID" from "products" where "ProductName"='Bilgisayar Y Z'
-
-
-SELECT  Distinct "public"."customers"."CustomerID",
-         "public"."customers"."CompanyName",
-         "public"."customers"."ContactName"
-FROM     "orders" 
-INNER JOIN "customers"  ON "orders"."CustomerID" = "customers"."CustomerID" 
-INNER JOIN "order_details"  ON "order_details"."OrderID" = "orders"."OrderID" 
-where "order_details"."ProductID" =
-(select "ProductID" from "products" where "ProductName"='Y Z Bilgisayar')
-
-order by "public"."customers"."CustomerID"
+-- Örnek
 
 
+SELECT "ProductID" FROM "products" WHERE "ProductName" = 'Bilgisayar Y Z';
 
 
-
-select "ProductID" from "products" where "ProductName" like 'A%'
-
-SELECT  Distinct "public"."customers"."CustomerID",
-         "public"."customers"."CompanyName",
-         "public"."customers"."ContactName"
-FROM     "orders" 
-INNER JOIN "customers"  ON "orders"."CustomerID" = "customers"."CustomerID" 
-INNER JOIN "order_details"  ON "order_details"."OrderID" = "orders"."OrderID" 
-where "order_details"."ProductID" IN
-(select "ProductID" from "products" where "ProductName" like 'A%')
+SELECT DISTINCT "public"."customers"."CustomerID",
+    "public"."customers"."CompanyName",
+    "public"."customers"."ContactName"
+FROM "orders" 
+INNER JOIN "customers" ON "orders"."CustomerID" = "customers"."CustomerID" 
+INNER JOIN "order_details" ON "order_details"."OrderID" = "orders"."OrderID" 
+WHERE "order_details"."ProductID" =
+    (SELECT "ProductID" FROM "products" WHERE "ProductName" = 'Y Z Bilgisayar')
+ORDER BY "public"."customers"."CustomerID";
 
 
+-- Örnek
+
+SELECT "ProductID" FROM "products" WHERE "ProductName" LIKE 'A%';
+
+SELECT DISTINCT "public"."customers"."CustomerID",
+    "public"."customers"."CompanyName",
+    "public"."customers"."ContactName"
+FROM "orders" 
+INNER JOIN "customers" ON "orders"."CustomerID" = "customers"."CustomerID" 
+INNER JOIN "order_details" ON "order_details"."OrderID" = "orders"."OrderID" 
+WHERE "order_details"."ProductID" IN
+    (SELECT "ProductID" FROM "products" WHERE "ProductName" LIKE 'A%');
+
+
+-- Örnek
 
 
 SELECT * FROM  "products"
@@ -112,7 +123,7 @@ WHERE "UnitPrice" = ANY
     LEFT OUTER JOIN "products" 
     ON "suppliers"."SupplierID" = "products"."SupplierID"
     WHERE "suppliers"."CompanyName" = 'Tokyo Traders'
-)
+);
 
 
 SELECT * FROM  "products"
@@ -123,7 +134,7 @@ WHERE "UnitPrice" IN
     LEFT OUTER JOIN "products" 
     ON "suppliers"."SupplierID" = "products"."SupplierID"
     WHERE "suppliers"."CompanyName" = 'Tokyo Traders'
-)
+);
 
 
 SELECT * FROM  "products"
@@ -134,7 +145,8 @@ WHERE "UnitPrice" < ANY
     LEFT OUTER JOIN "products" 
     ON "suppliers"."SupplierID" = "products"."SupplierID"
     WHERE "suppliers"."CompanyName" = 'Tokyo Traders'
-)
+);
+
 
 SELECT * FROM  "products"
 WHERE "UnitPrice" < ALL 
@@ -144,61 +156,60 @@ WHERE "UnitPrice" < ALL
     LEFT OUTER JOIN "products" 
     ON "suppliers"."SupplierID" = "products"."SupplierID"
     WHERE "suppliers"."CompanyName" = 'Tokyo Traders'
-)
+);
 
+
+-- Örnek
 
 
 SELECT "SupplierID", SUM("UnitsInStock") AS "Stoktaki Toplam Ürün Sayısı" 
 FROM  "products"
 GROUP BY "SupplierID"
-HAVING SUM("UnitsInStock") < (SELECT AVG("UnitsInStock") FROM "products")
+HAVING SUM("UnitsInStock") < (SELECT AVG("UnitsInStock") FROM "products");
+
+
+-- Örnek
+
+
+SELECT MAX("Quantity") FROM "order_details";
+
+
+SELECT "ProductID", SUM("Quantity")
+FROM "order_details"
+GROUP BY "ProductID"
+HAVING SUM("Quantity") > (SELECT MAX("Quantity") FROM "order_details");
+
+
+-- Örnek
+
+
+SELECT 
+    "ProductName", 
+    "UnitsInStock", 
+    (SELECT MAX("UnitsInStock") FROM "products") AS "En Büyük Değer" 
+FROM "products";
+
+
+SELECT 
+    "SupplierID", 
+    COUNT("UnitsInStock") AS "Toplam",
+    SQRT(SUM(("UnitsInStock" - (SELECT AVG("UnitsInStock") FROM "products")) ^ 2) / COUNT("UnitsInStock"))  AS "Standart Sapma" 
+FROM "products" 
+GROUP BY "SupplierID"
+
+
+-- Örnek
+
+
+SELECT "ProductName", "UnitPrice" FROM "products" AS "urunler1"
+WHERE "urunler1"."UnitPrice" > 
+(
+SELECT AVG("UnitPrice") FROM "products" AS "urunler2" 
+WHERE "urunler1"."SupplierID" = "urunler2"."SupplierID"
+);
+
 
 SELECT "CustomerID", "CompanyName", "ContactName" 
 FROM "customers"
 WHERE EXISTS
-(
-       SELECT * FROM "orders" WHERE "customers"."CustomerID" = "orders"."CustomerID"
-)
-
-
-
-
-select avg("Quantity") from "order_details"
-
-select "ProductID",sum("Quantity")
-from "order_details"
-group by "ProductID"
-having sum("Quantity")> (select max("Quantity") from "order_details"
-)
-
-
-SELECT 
-"ProductName", 
-"UnitsInStock", 
-(SELECT MAX("UnitsInStock") FROM "products") AS "En Büyük Değer" 
-FROM "products
-
-
-
-SELECT "SupplierID", 
-COUNT("UnitsInStock") AS "Toplam",
-sqrt(SUM(("UnitsInStock" - (SELECT AVG("UnitsInStock") FROM "products")) ^ 2) / COUNT("UnitsInStock"))  AS "Standart Sapma" 
-FROM "products"GROUP BY "SupplierID"
-
-
-
-SELECT "ProductName", "UnitPrice" FROM "products" AS "urunler1”
-WHERE "urunler1"."UnitPrice" > 
-(
-SELECT AVG("UnitPrice") FROM "products" AS "urunler2" 
-WHERE "urunler1"."SupplierID" = "urunler2"."SupplierID”
-)
-
-
-SELECT "CustomerID", "CompanyName", "ContactName" 
-FROM "customers”
-WHERE EXISTS
-(
-SELECT * FROM "orders" WHERE "customers"."CustomerID" = "orders"."CustomerID”
-)
-
+    (SELECT * FROM "orders" WHERE "customers"."CustomerID" = "orders"."CustomerID");
