7df3f99232ea579b10ea36b60253b2384c7a43e5
celalceken <celalceken@gmail.com>
2017-10-22 23:15:37 +0300

    Te mel SQL (SQL DML Komutları; Select, Insert, Update, Delete, Join

--- VeritabaniYonetimSistemleri6/SQLSorgulari6.sql
+++ VeritabaniYonetimSistemleri6/SQLSorgulari6.sql
@@ -1,14 +1,30 @@
 
+--***********Aşağıdaki sorgular NorthWind Örnek Veritabanını Kullanmaktadır.********--
+
+--**************** SELECT ********************--
+
+-- Select Komutu veritabanından veri almak (arama/listeleme) için kullanılır.
 
 SELECT * FROM "customers";
 
 SELECT "CompanyName", "ContactName" FROM "customers";
 
+
+-- İstenilen koşula uyan kayıtların listelenmesi için WHERE komutu kullanılır.
+
+
 SELECT * FROM "customers" WHERE "Country" = 'Germany';
 
 SELECT * FROM "customers" WHERE "Country"='Germany' and "City" = 'Berlin';
 
+
+--Tabloda bazı sütunlar tekrar eden kayıtlar içerebilir. “DISTINCT” ifadesi sorgu sonucu gelen değerler
+-- içerisindeki tekrarlanan kayıtların tek kayıt olarak gösterilmesini sağlar.
+
 SELECT DISTINCT "City" from "customers";
+
+-- Sorgular sonucunda listelenen kayıtların belirli alanlara göre alfabetik veya sayısal olarak
+-- artan yada azalan şeklinde sıralanması için “ORDER BY” komutu kullanılır.
 
 SELECT * FROM "customers" ORDER BY "ContactName" ASC;
 
@@ -112,7 +128,55 @@ RIGHT OUTER JOIN "employees" on "orders"."EmployeeID" = "employees"."EmployeeID"
 
 -----------------------------
 
+--Bir tablodan alınan verileri, yeni bir tabloya kopyalamak için kullanılır. Yeni tablonun
+--mevcut olmaması gerekir.
 
 SELECT "CompanyName", "ContactName" INTO "MusteriYedek" FROM "customers";
 
-INSERT INTO "MusteriYedek" SELECT "CompanyName", "ContactName" FROM "customers";
+
+
+--INSERT komutu tabloya yeni kayıt eklemek için kullanılır. 
+--Ekleme işlemlerinde veri bütünlüğü kısıtları göz önüne alınır.
+
+INSERT INTO Customers ("CustomerName", "ContactName","Address", "City", "PostalCode", "Country")
+VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');
+
+--Yalnızca bazı sütunlara veri eklememizde mümkündür. Veri eklenmeyen sütunlar null (boş)
+--gözükecektir.
+
+INSERT INTO Customers ("CustomerName ", " City ", " Country ")
+VALUES ('Cardinal','Stavanger','Norway');
+
+
+-- Bir tablodan alınan verileri, varolan bir tabloya kopyalamak için kullanılır.
+
+INSERT INTO "MusteriYedek" SELECT "CompanyName", "ContactName" FROM "customers"; 
+
+
+
+
+--UPDATE komutu tablodaki kayıt(lar)ın değiştirilmesini sağlar.
+--Güncelleme işlemlerinde veri bütünlüğü kısıtları göz önüne alınır.
+
+UPDATE Customers
+SET ContactName='Alfred Schmidt', City='Hamburg'
+WHERE CustomerName='Alfreds Futterkiste';  --WHERE ifadesi kullanılmazsa tüm satırlar değiştirilir.--
+
+
+
+
+
+--DELETE ifadesi tablodaki kayıt veya kayıtların silinmesini sağlar.
+--Silme işlemlerinde veri bütünlüğü kısıtları göz önüne alınır.
+
+DELETE FROM Customers
+WHERE CustomerName='Alfreds Futterkiste' AND ContactName='Maria Anders';
+
+
+--Tabloyu silmeden tablodaki bütün satırları silmek mümkündür. Aşağıdaki komut tablodaki
+bütün kayıtları silmeye yarar.
+DELETE FROM table_name;
+
+
+
+
